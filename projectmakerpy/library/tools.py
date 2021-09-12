056e6faeb8e1afc8af0f265fd1e187f2f5b09ffd
Fabrice Quenneville <fabquenneville@gmail.com>
2021-09-12 05:03:49 -0400

    Building docsource automatically now working

--- projectmakerpy/library/tools.py
+++ projectmakerpy/library/tools.py
@@ -8,6 +8,7 @@ import sys
 import stat
 
 from datetime import datetime
+from shutil import copyfile
 
 def load_arguments():
     '''Get/load command parameters 
@@ -21,17 +22,23 @@ def load_arguments():
         "path":str(),
         "name":str(),
         "license":False,
+        "docs":True,
         "owner":str(),
     }
 
     for arg in sys.argv:
         # Confirm with the user that he selected to delete found files
         if "-path:" in arg:
-            arguments["path"] = arg[6:]
+            path = arg[6:]
+            if path[-1:] != "/":
+                path = path + "/"
+            arguments["path"] = path
         elif "-name:" in arg:
             arguments["name"] = arg[6:]
         elif "-license:" in arg:
             arguments["license"] = arg[9:]
+        elif "-nodocs" in arg:
+            arguments["docs"] = False
         elif "-owner:" in arg:
             arguments["owner"] = arg[7:]
 
@@ -46,39 +53,29 @@ def make_directory(path):
     print (f"Successfully created {path}")
     return True
 
-def make_empty(path):
+def make_empty_file(path):
     open(path, 'a').close()
     print (f"Successfully created {path}")
 
 def make_gitignore(path):
     templatespath = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/templates/"
-    with open(templatespath + "gitignore.txt") as fi:
-        with open(path + '.gitignore', 'w') as fo:
-            for line in fi:
-                fo.write(line)
-    print (f"Successfully created {path + '.gitignore'}")
+    copyfilled(templatespath + "gitignore.txt", path + '.gitignore')
 
 def make_todo(path):
     templatespath = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/templates/"
 
-    with open(templatespath + "todo.md") as fi:
-        with open(path + 'TODO.md', 'w') as fo:
-            for line in fi:
-                fo.write(line)
+    copyfilled(templatespath + "todo.md", path + 'TODO.md')
 
-    print (f"Successfully created {path + 'TODO.md'} ")
-
-def make_readme(path, name):
+def make_readme(path, projectname):
     templatespath = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/templates/"
 
-    with open(templatespath + "readme.md") as fi:
-        with open(path + 'README.md', 'w') as fo:
-            for line in fi:
-                fo.write(line.replace("projectname", name))
+    copyfilled(
+        pathin          = templatespath + "readme.md",
+        pathout         = path + 'README.md',
+        projectname     = projectname
+    )
 
-    print (f"Successfully created {path + 'TODO.md'} ")
-
-def make_license(path, owner, license = False):
+def make_license(path, projectowner, license = False):
     templatespath = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/templates/"
 
     licensefname = "licenses/mit.txt"
@@ -91,41 +88,86 @@ def make_license(path, owner, license = False):
     elif license == "apache":
         licensefname = "licenses/apache.txt"
 
-    with open(templatespath + licensefname) as fi:
-        with open(path + 'LICENSE', 'w') as fo:
-            for line in fi:
-                fo.write(line.replace("projectyear", str(datetime.now().year)).replace("projectowner", owner))
+    copyfilled(
+        pathin          = templatespath + licensefname,
+        pathout         = path + 'LICENSE',
+        projectowner    = projectowner,
+        projectyear     = str(datetime.now().year)
+    )
 
-    print (f"Successfully created {path + 'LICENSE'} ")
-
-def make_setup(path, owner, name):
+def make_setup(path, projectowner, projectname):
     templatespath = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/templates/"
     fileoutpath = path + 'setup.py'
 
-    with open(templatespath + "setup.py") as fi:
-        with open(fileoutpath, 'w') as fo:
-            for line in fi:
-                fo.write(line.replace("projectname", name).replace("projectowner", owner))
-    print (f"Successfully created {fileoutpath}")
-
-    st = os.stat(fileoutpath)
-    os.chmod(fileoutpath, st.st_mode | stat.S_IXUSR | stat.S_IXGRP)
-    print (f"Gave executable permission to {fileoutpath}")
+    copyfilled(
+        pathin          = templatespath + "setup.py",
+        pathout         = fileoutpath,
+        projectname     = projectname,
+        projectowner    = projectowner
+    )
 
 def make_main(path, name):
     templatespath = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/templates/"
     fileoutpath = path + name + '.py'
 
-    with open(templatespath + "helloworld.py") as fi:
-        with open(fileoutpath, 'w') as fo:
-            for line in fi:
-                fo.write(line)
-    print (f"Successfully created {fileoutpath}")
-
-    st = os.stat(fileoutpath)
-    os.chmod(fileoutpath, st.st_mode | stat.S_IXUSR | stat.S_IXGRP)
-    print (f"Gave executable permission to {fileoutpath}")
+    copyfilled(templatespath + "helloworld.py", fileoutpath)
 
     with open(path + '__init__.py', 'w') as f:
         f.write(f"__all__ = ['{name}']")
     print (f"Successfully created {path + '__init__.py'}")
+
+
+
+def make_docs(path, projectowner, projectname):
+    templatespath = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/templates/docsource"
+
+    # Making folders
+    origfolders = [result[0] for result in os.walk(templatespath)]
+    origfolders = [folder for folder in origfolders if folder[len(templatespath)+1:len(templatespath) + 5] != ".git"]
+    folderstomake = [folder.replace(templatespath, "") for folder in origfolders if folder.replace(templatespath, "") != ""]
+    for i in range(len(folderstomake)):
+        if folderstomake[i][:1] == "/":
+            folderstomake[i] = folderstomake[i][1:]
+    for folder in folderstomake:
+        make_directory(path + "docsource/" + folder)
+    
+    # Copying files
+    for folder in origfolders:
+        files = os.listdir(folder)
+        for filename in files:
+            if filename[-4:] in [".git", ]:
+                continue
+            if filename[-10:] in [".gitignore", ]:
+                continue
+            if not os.path.isfile(os.path.join(folder, filename)):
+                continue
+            copyfilled(
+                pathin          = folder + "/" + filename,
+                pathout         = path + "docsource/" + folder.replace(templatespath, "")[1:] + filename,
+                projectname     = projectname,
+                projectowner    = projectowner
+            )
+            # print(folder + "/" + filename)
+            # print(path + "docsource/" + folder.replace(templatespath, "")[1:] + filename)
+
+def copyfilled(pathin, pathout, projectowner = False, projectname = False, projectyear = False):
+    try:
+        with open(pathin) as fi:
+            with open(pathout, 'w') as fo:
+                text = fi.read()
+                if projectowner:
+                    text = text.replace("projectowner", projectowner)
+                if projectname:
+                    text = text.replace("projectname", projectname)
+                if projectyear:
+                    text = text.replace("projectyear", projectyear)
+                fo.write(text)
+    except UnicodeDecodeError:
+        copyfile(pathin, pathout)
+    print (f"Successfully created {pathout}")
+    if pathout[-2:] in ["sh", "py"] or pathout[-3:] in ["bat"]:
+        st = os.stat(pathout)
+        os.chmod(pathout, st.st_mode | stat.S_IXUSR | stat.S_IXGRP)
+        print (f"Gave executable permission to {pathout}")
+
+        
--- projectmakerpy/projectmakerpy.py
+++ projectmakerpy/projectmakerpy.py
@@ -3,44 +3,60 @@
 
 # Normal import
 try:
-    from projectmakerpy.library.tools import load_arguments, make_directory, make_gitignore, make_todo, make_readme, make_main, make_setup, make_empty, make_license
+    from projectmakerpy.library.tools import load_arguments, make_directory, make_gitignore, make_todo, make_readme, make_main, make_setup, make_empty_file, make_license, make_docs
 # Allow local import for development purposes
 except ModuleNotFoundError:
-    from library.tools import load_arguments, make_directory, make_gitignore, make_todo, make_readme, make_main, make_setup, make_empty, make_license
+    from library.tools import load_arguments, make_directory, make_gitignore, make_todo, make_readme, make_main, make_setup, make_empty_file, make_license, make_docs
 
 
 def main():
 
     # Get/load command parameters
     arguments = load_arguments()
-
     if len(arguments["path"]) < 1 or len(arguments["name"]) < 1:
         return False
-
-    if arguments["path"][-1:] != "/":
-        arguments["path"] = arguments["path"] + "/"
-    
     if not make_directory(arguments["path"] + arguments["name"]):
-        exit()
-    
+        return False
+
+    # Setting variables    
     projectpath = arguments["path"] + arguments["name"] + "/"
     codepath = projectpath + arguments["name"] + "/"
 
+    # Making main folders
     make_directory(projectpath + "dist")
-    make_directory(projectpath + "docs")
-    make_directory(projectpath + "docsource")
     make_directory(projectpath + "misc")
     make_directory(projectpath + arguments["name"])
     make_directory(projectpath + arguments["name"] + "/library")
 
+    # Making components
     make_gitignore(projectpath)
     make_todo(projectpath)
-    make_readme(projectpath, arguments["name"])
-    make_setup(projectpath, arguments["owner"], arguments["name"])
-    make_license(projectpath, arguments["owner"], arguments["license"])
-    make_empty(projectpath + "requirements.txt")
-    
+    make_readme(
+        path            = projectpath,
+        projectname     = arguments["name"]
+    )
+    make_setup(
+        path            = projectpath,
+        projectowner    = arguments["owner"],
+        projectname     = arguments["name"]
+    )
+    make_license(
+        path            = projectpath,
+        projectowner    = arguments["owner"],
+        license         = arguments["license"]
+    )
+    make_empty_file(projectpath + "requirements.txt")
     make_main(codepath, arguments["name"])
+
+    # Documentation
+    make_directory(projectpath + "docs")
+    make_directory(projectpath + "docsource")
+    if arguments["docs"]:
+        make_docs(
+            path            = projectpath, 
+            projectowner    = arguments["owner"], 
+            projectname     = arguments["name"]
+        )
 
 
 if __name__ == '__main__':
